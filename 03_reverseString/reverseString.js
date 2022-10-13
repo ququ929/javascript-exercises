061160ef14e5c686172adfdef9e4b26769e2662c
ququ929 <ququ929@hotmail.com>
2022-10-13 22:29:44 +0800

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let char = "";
+    let newWord = "";
+    let wordLength = word.length;
 
-};
+    for (let i = wordLength; i >= 0; i--) {
+        char = word.charAt(i);
+        newWord += char;
+    }
+
+    return newWord;
+}
+
+// reverseString("test");
 
 // Do not edit below this line
 module.exports = reverseString;
