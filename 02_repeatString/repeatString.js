f5c79abb7a8a050a4bb862f3d3820b4ef5fe78c5
ququ929 <ququ929@hotmail.com>
2022-10-13 21:58:47 +0800

    Complete Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let result = "";
 
+    if (num < 0) return "ERROR";
+
+    while (num) {
+        result += string;
+        num--;
+    }
+
+    return result;    
 };
 
 // Do not edit below this line
