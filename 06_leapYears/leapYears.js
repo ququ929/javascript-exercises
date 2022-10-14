0a18750534b1a422fa2c7708b8e8e5dd6545e7e7
ququ929 <ququ929@hotmail.com>
2022-10-15 00:33:37 +0800

    Complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,12 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // year % 4 === 0, True, third
+    // year % 100 === 0 False, second
+    // year % 400 === 0 True, first condition to check
+
+    if (year % 400 === 0) return true;
+    else if (year % 100 === 0) return false;
+    else if (year % 4 === 0) return true;
+    else return false;
 
 };
 
