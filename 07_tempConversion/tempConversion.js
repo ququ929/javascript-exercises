1a13d3b8455752829ef62ba769f724eaf4889034
ququ929 <ququ929@hotmail.com>
2022-10-18 21:09:36 +0800

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,18 @@
-const ftoc = function() {
-
+// Math.round(n * 10)/10, round to first decimal place
+// XF = (X - 32) * 5 / 9 C
+const ftoc = function(fahrenheit) {
+	fd = (fahrenheit - 32) * 5 / 9;
+	return Math.round(fd * 10) / 10;
 };
 
-const ctof = function() {
-
+// XC = (X * 9 / 5 + 32) F
+const ctof = function(celsius) {
+	cd = celsius * 9 / 5 + 32;
+	return Math.round(cd * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+	ftoc,
+	ctof
 };
