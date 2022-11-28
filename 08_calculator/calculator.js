28975daaca5cc71d657b874a12785e78c24608ec
ququ929 <ququ929@hotmail.com>
2022-11-28 11:21:42 +0800

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  // reduce method:
+  return numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+//   for of method:  
+//   let total = 0;
+//   for (let n of numbers) {
+//     total += +n;
+//   }
+//   return total;
 };
 
-const multiply = function() {
-
+const multiply = function(numberArray) {
+  return numberArray.reduce((accumulator, currentValue) => accumulator * currentValue);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(n) {
+  if (n === 0) return 1;
+  
+  else {
+    const index = [...Array(n).keys()];
+    const numbers = index.map(n => n + 1);
+    return numbers.reduce((accumulator, currentValue) => accumulator * currentValue);
+  }
+}
 
 // Do not edit below this line
 module.exports = {
