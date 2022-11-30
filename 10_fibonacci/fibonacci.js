b15d20fc7ced145b74364e36509d511e74d689c8
ququ929 <ququ929@hotmail.com>
2022-11-30 22:29:13 +0800

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = +n; // accept string
 
+    if (n < 0) return "OOPS";
+    
+    let fibList = [1, 1];
+
+    // add new element to the array, which equals to sum of last 2 number in the array.
+    // loop for n - 2 times(we already have 2 element in list)
+    for (let i = n - 2; i > 0; i--) {
+        fibList.push(fibList.at(-1) + fibList.at(-2));
+    }
+
+    return fibList.at(-1);
 };
 
 // Do not edit below this line
