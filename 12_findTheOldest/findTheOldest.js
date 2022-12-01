6c0c756753ce12ade8307db8b48a7ac65647671d
ququ929 <ququ929@hotmail.com>
2022-12-01 23:56:08 +0800

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
 
+    array.forEach(function(person) {
+        if (person.yearOfDeath === undefined) {
+        person.yearOfDeath = new Date().getFullYear();
+        }
+     });
+
+    const ageArray = array.sort(function (a, b) {
+        const aAge = a.yearOfDeath - a.yearOfBirth;
+        const bAge = b.yearOfDeath - b.yearOfBirth;
+
+        return bAge - aAge;
+    });
+    
+    return ageArray[0];
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
